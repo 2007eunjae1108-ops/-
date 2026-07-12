e39a75ad374b16ecb07e3b56075971fe632e98e9
2007eunjae1108-ops <2007eunjae1108@gmail.com>
2026-07-12 17:09:49 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,26 +4,30 @@ import {
 } from "./firebase.js";
 
 import {
-  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-// 로그인
+
 export async function login() {
+  await signInWithRedirect(auth, provider);
+}
+
+export async function checkRedirectResult() {
   try {
-    await signInWithPopup(auth, provider);
+    await getRedirectResult(auth);
   } catch (e) {
     alert(e.code + "\n\n" + e.message);
   }
 }
-// 로그인 상태 확인
+
 export function observeAuth(callback) {
   onAuthStateChanged(auth, (user) => {
     callback(user);
   });
 }
 
-// 로그아웃
 export async function logout() {
   await signOut(auth);
 }
